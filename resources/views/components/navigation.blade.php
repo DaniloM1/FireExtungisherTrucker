98691a2f19dd6e42ab42285c9b5bc66d9b23c83f
Danilo Milanovic <danilo.milanovic@centrosinergija.rs>
2025-07-15 15:51:48 +0200

    izmene nav

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -175,7 +175,7 @@
                         </a>
                     @endauth
                 @endif
-                <button class="mt-2 w-full bg-red-600 text-white px-4 py-2 rounded-full hover:bg-red-700 transition duration-300">
+                <button  class="mt-2 w-full bg-red-600 text-white px-4 py-2 rounded-full hover:bg-red-700 transition duration-300">
                     Zatraži Ponudu
                 </button>
             </div>
